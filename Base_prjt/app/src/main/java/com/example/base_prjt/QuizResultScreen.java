c845fdf37a3fd09e42d30780ec2ed9c80f95d897
Kirill <36568961+sigseg5@users.noreply.github.com>
2019-10-26 22:25:55 +0300

    Fixed crash at second activity

--- Base_prjt/app/src/main/java/com/example/base_prjt/QuizResultScreen.java
+++ Base_prjt/app/src/main/java/com/example/base_prjt/QuizResultScreen.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageView;
 
 import android.os.Bundle;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 public class QuizResultScreen extends AppCompatActivity {
@@ -17,19 +16,22 @@ public class QuizResultScreen extends AppCompatActivity {
     String placeholder = "";
     int moneyPerMonth = 123;
 
-    AppCompatImageView resultImageOutlet = findViewById(R.id.resultImage);
-    TextView resultTextOutlet = findViewById(R.id.resultText);
-    TextView userAnswerTextoutlet = findViewById(R.id.userAnswerText);
-    TextView realSumPercentTextOutlet = findViewById(R.id.realSumPercentText);
-    TextView peopleNameTextOutlet = findViewById(R.id.peopleNameText);
-    TextView yearOfDeclarationTextOutlet = findViewById(R.id.yearOfDeclarationText);
-    TextView realSumMoneyTextOutlet = findViewById(R.id.realSumMoneyText);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_result_screen);
 
+        AppCompatImageView resultImageOutlet = findViewById(R.id.resultImage);
+        TextView resultTextOutlet = findViewById(R.id.resultText);
+        TextView userAnswerTextOutlet = findViewById(R.id.userAnswerText);
+        TextView realSumPercentTextOutlet = findViewById(R.id.realSumPercentText);
+        TextView peopleNameTextOutlet = findViewById(R.id.peopleNameText);
+        TextView yearOfDeclarationTextOutlet = findViewById(R.id.yearOfDeclarationText);
+        TextView realSumMoneyTextOutlet = findViewById(R.id.realSumMoneyText);
 
+//        double tenPercent = moneyPerMonth / 100 * 10;
+//        System.out.println(tenPercent);
     }
 }
