a88f32defe4fe8f7d7ce2e0fed372d77c023ddd3
Kirill <36568961+sigseg5@users.noreply.github.com>
2019-10-26 23:08:40 +0300

    Added content complition to Second View

--- Base_prjt/app/src/main/java/com/example/base_prjt/QuizResultScreen.java
+++ Base_prjt/app/src/main/java/com/example/base_prjt/QuizResultScreen.java
@@ -3,18 +3,21 @@ package com.example.base_prjt;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageView;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.text.MessageFormat;
+
 public class QuizResultScreen extends AppCompatActivity {
 
-    int userAnswer = 13546;
-    double realSumPercent = 12.2; //round value
-    String peopleName = "Putin V.V.";
-    int yearOfDeclaration = 2019;
-    int realSumMoney = 121;
-    String placeholder = "";
-    int moneyPerMonth = 123;
+    int userAnswer = 130; //ответ пользователя
+    double realSumPercent = 12.2; //на сколько процентов его реальные доходы больше/меньше
+    String peopleName = "Путин В.В."; //ФИО чиновника
+    int yearOfDeclaration = 2019; //год подачи декларации
+    int realSumMoney = 121973; //размер всего состояния
+    int moneyPerMonth = 123; //доход в месяц
+    String placeholder = ""; // больше/меньше
 
 
 
@@ -31,7 +34,30 @@ public class QuizResultScreen extends AppCompatActivity {
         TextView yearOfDeclarationTextOutlet = findViewById(R.id.yearOfDeclarationText);
         TextView realSumMoneyTextOutlet = findViewById(R.id.realSumMoneyText);
 
-//        double tenPercent = moneyPerMonth / 100 * 10;
-//        System.out.println(tenPercent);
+        double tenPercent = moneyPerMonth / 100 * 10;
+
+        if (    (userAnswer >= (moneyPerMonth - tenPercent) && (userAnswer <= moneyPerMonth + tenPercent))  ) {
+            resultImageOutlet.setBackgroundResource(R.drawable.trophy);
+            resultTextOutlet.setText("Угадали");
+            resultTextOutlet.setTextColor(Color.parseColor("#24FF00"));
+        } else {
+            resultImageOutlet.setBackgroundResource(R.drawable.error);
+            resultTextOutlet.setText("Не угадали");
+            resultTextOutlet.setTextColor(Color.parseColor("#E43F3F"));
+        }
+
+        userAnswerTextOutlet.setText(MessageFormat.format("Ваш вариант: {0}₽", userAnswer));
+
+        if (userAnswer < moneyPerMonth) {
+            placeholder = "больше";
+        } else {
+            placeholder = "меньше";
+        }
+
+        realSumPercentTextOutlet.setText(MessageFormat.format("Реальная сумма на {0}% {1}", realSumPercent, placeholder));
+        peopleNameTextOutlet.setText(peopleName);
+        yearOfDeclarationTextOutlet.setText(MessageFormat.format("Его доход за месяц {0}г.", yearOfDeclaration));
+        realSumMoneyTextOutlet.setText(MessageFormat.format("{0}₽", realSumMoney));
+
     }
 }
